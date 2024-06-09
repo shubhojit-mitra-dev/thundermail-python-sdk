8669d7160934ae74ed5351ea8a87d7b4a6dd2d3d
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2024-06-09 12:04:37 +0530

    create setup.py and version.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+from thundermail.version import get_version
+
+install_requires = open("requirements.txt").readlines()
+
+setup(
+    name="thundermail",
+    version=get_version(),
+    description="Thundermail Python SDK",
+    long_description=open("README.md", encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    author="shubhojit-mitra-dev",
+    author_email="mitrashubhojit2005@gmail.com",
+    url="https://github.com/shubhojit-mitra-dev/thundermail-python-sdk/",
+    python_requires=">=3.7",
+    keywords=["email","email platform"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
---
+++ thundermail/version.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.1"
+
+def get_version():
+    """ returns the version of thundermail sdk """
+    return __version__
